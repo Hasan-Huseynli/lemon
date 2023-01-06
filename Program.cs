c174833d84ba68c08dfeb73176458a32360a3d5e
Hasan Huseynli <hasan@Hasans-MacBook-Pro.local>
2023-01-06 09:20:25 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,351 @@
+﻿namespace lemon;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+
+        //question 1
+
+
+        //    Console.Write("Enter a four-digit number:");
+        //label1:
+        //    string inputValue = Console.ReadLine();
+
+        //    int reqem;
+        //    if (int.TryParse(inputValue, out reqem) != true)
+        //    {
+        //        Console.WriteLine("reqem daxil etmediniz");
+        //        goto label1;
+        //    }
+
+        //    if (reqem >= 1000 && reqem < 10000)
+        //    {
+        //        Console.WriteLine((70000 + reqem) * 10 + 8);
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("4 reqemli daxil etmediniz");
+        //        goto label1;
+        //    }
+        //    Console.ReadKey();
+
+
+
+
+
+
+
+
+
+        //question2
+
+
+        //    Console.Write("Enter a three-digit number:");
+        //label1:
+        //    string inputValue = Console.ReadLine();
+
+        //    int reqem;
+        //    if (int.TryParse(inputValue, out reqem) != true)
+        //    {
+        //        Console.WriteLine("reqem daxil etmediniz");
+        //        goto label1;
+        //    }
+
+        //    if (reqem >= 100 && reqem < 1000)
+        //    {
+        //        Console.WriteLine(reqem * 1000 + reqem);
+
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("3 reqemli daxil etmediniz");
+        //        goto label1;
+        //    }
+        //    Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+        //question3
+
+
+        //    Console.Write("Enter a five-digit number:");
+        //label1:
+        //    string inputValue = Console.ReadLine();
+
+        //    int reqem;
+        //    if (int.TryParse(inputValue, out reqem) != true)
+        //    {
+        //        Console.WriteLine("reqem daxil etmediniz");
+        //        goto label1;
+        //    }
+
+        //    if (reqem >= 10000 && reqem < 100000)
+        //    {
+        //        Console.WriteLine((reqem * 18 / 100) * 3 / 100);
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("5 reqemli daxil etmediniz");
+        //        goto label1;
+        //    }
+        //    Console.ReadKey();
+
+
+
+
+
+
+
+
+        //question4
+
+
+        //    Console.Write("Enter a three-digit number:");
+        //label1:
+        //    string inputValue = Console.ReadLine();
+
+        //    int reqem;
+        //    if (int.TryParse(inputValue, out reqem) != true)
+        //    {
+        //        Console.WriteLine("reqem daxil etmediniz");
+        //        goto label1;
+        //    }
+
+        //    if (reqem >= 100 && reqem < 1000)
+        //    {
+        //        Console.WriteLine((reqem * 10 + 7) * 7 / 100);
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("3 reqemli daxil etmediniz");
+        //        goto label1;
+        //    }
+        //    Console.ReadKey();
+
+
+
+
+
+
+
+
+        //question 5
+
+        //int reqem = ReadInt64("Enter a five-digit number:", 4);
+        //Console.WriteLine(((40000 + reqem) * 100 + 44) * 44 / 100);
+        //Console.ReadKey();
+
+
+
+
+
+
+
+        //question6
+
+        //int reqem = ReadInt32("Enter a five-digit number:", 4);
+        //int cavab = (reqem * 20 / 100) * 10 / 100;
+        //Console.WriteLine(cavab * cavab);
+        //Console.ReadKey();
+
+
+
+
+
+
+
+
+        //question7
+
+        //int birinciReqem = ReadInt64("Enter first five-digit number:", 5);
+        //int ikinciReqem = ReadInt64("Enter second five-digit number:", 5);
+        //int cavab = birinciReqem + ikinciReqem;
+        //if (cavab < 100000)
+        //{
+        //    cavab = 500000 + cavab;
+        //}
+        //else
+        //{
+        //    cavab = 5000000 + cavab;
+        //}
+        //cavab = cavab * 10 + 5;
+        //cavab = cavab * 5 / 100;
+        //Console.WriteLine(cavab);
+        //Console.ReadKey();
+
+
+
+
+        //Test 8
+        //int birinciReqem = ReadInt64("Enter four-digit number:", 4);
+        //int ikinciReqem = ReadInt64("Enter seven-digit number:", 7);
+        //int cavab = birinciReqem * 4 / 100;
+        //int cavab2 = ikinciReqem * 9 / 100;
+        //int netice = (cavab + cavab2) * 10 / 100;
+        //Console.WriteLine(netice);
+        //Console.ReadKey();
+
+
+
+
+        //Test 9
+        //int birinciReqem = ReadInt64("Enter first six-digit number:", 6);
+        //int ikinciReqem = ReadInt64("Enter second six-digit number:", 6);
+        //int ucuncuReqem = ReadInt64("Enter third six-digit number:", 6);
+        //int cavab1 = birinciReqem * 10 / 100;
+        //int cavab2 = ikinciReqem * 10 / 100;
+        //int cavab3 = ucuncuReqem * 10 / 100;
+        //int netice= (cavab1 + cavab2 + cavab3) * 10 / 100;
+        //Console.WriteLine(netice);
+        //Console.ReadKey();
+
+
+
+
+        //Test 10
+        //int birinciReqem = ReadInt64("Enter first four-digit number:", 4);
+        //int ikinciReqem = ReadInt64("Enter second four-digit number:", 4);
+        //int ucuncuReqem = ReadInt64("Enter third four-digit number:", 4);
+        //int cavab1 = birinciReqem * 1 / 100;
+        //int cavab2 = ikinciReqem * 2 / 100;
+        //int cavab3 = ucuncuReqem * 3 / 100;
+        //int subtraction = cavab1 - cavab2 - cavab3;
+        //int _7percentofnumbers = cavab1 + cavab2 + cavab3;
+        //int netice = subtraction + _7percentofnumbers;
+        //Console.WriteLine(netice);
+        //Console.ReadKey();
+
+
+
+
+
+        //Test 11
+        //int birinciReqem = ReadInt64("Enter first four-digit number:", 5);
+        //int ikinciReqem = ReadInt64("Enter second four-digit number:", 5);
+        //int ucuncuReqem = ReadInt64("Enter third four-digit number:", 5);
+        //int dorduncuReqem = ReadInt64("Enter fourth four-digit number:", 5);
+        //int vurma1 = birinciReqem + ucuncuReqem;
+        //int vurma2 = ikinciReqem + dorduncuReqem;
+        //int hasil1ve2 = vurma1 * vurma2;
+        //int netice = hasil1ve2 - (ucuncuReqem * 3 / 100);
+        //Console.WriteLine(netice);
+        //Console.ReadKey();
+
+
+
+
+
+
+        //Test12
+        //int birinciReqem = ReadInt64("Enter first four-digit number:", 6);
+        //int ikinciReqem = ReadInt64("Enter second four-digit number:", 6);
+        //int ucuncuReqem = ReadInt64("Enter third four-digit number:", 6);
+        //int dorduncuReqem = ReadInt64("Enter fourth four-digit number:", 6);
+        //int firstnumper10 = birinciReqem * 10 / 100;
+        //int secondnumper10 = ikinciReqem * 10 / 100;
+        //int thirdnumper10 = ucuncuReqem * 10 / 100;
+        //int fourthnumper10 = dorduncuReqem * 10 / 100;
+        //int sum = firstnumper10 + secondnumper10+ thirdnumper10+ fourthnumper10;
+        //int firstnumper15 = birinciReqem * 15 / 100;
+        //int secondnumper15 = ikinciReqem * 15 / 100;
+        //int thirdnumper15 = ucuncuReqem * 15 / 100;
+        //int fourthnumper15 = dorduncuReqem * 15 / 100;
+        //int sum2 = firstnumper15 + secondnumper15 + thirdnumper15 + fourthnumper15;
+        //int netice = ((sum * sum2) * 10 / 100) * 11 / 100;
+        //Console.WriteLine(netice);
+        //Console.ReadKey();
+
+
+
+
+
+
+        //Test 13
+        //int birinciReqem = ReadInt64("Enter first four-digit number:", 5);
+        //int ikinciReqem = ReadInt64("Enter second four-digit number:", 5);
+        //int ucuncuReqem = ReadInt64("Enter third four-digit number:", 5);
+        //int dorduncuReqem = ReadInt64("Enter fourth four-digit number:", 3);
+        //int beshinciReqem= ReadInt64("Enter fifth four-digit number:", 3);
+        //int firstnumper15 = birinciReqem * 5 / 100;
+        //int secondnumper5 = ikinciReqem * 5 / 100;
+        //int thirdnumper5 = ucuncuReqem * 5 / 100;
+        //int multiplication = firstnumper15 * secondnumper5 * thirdnumper5;
+        //int fourthnumper15 = dorduncuReqem * 3 / 100;
+        //int fifthhnumper15 = beshinciReqem * 3 / 100;
+        //int sum = fourthnumper15 + fifthhnumper15;
+        //int netice = (multiplication * 10 / 100) + (sum * 10 / 100);
+        //Console.WriteLine(netice);
+        //Console.ReadKey();
+
+
+
+
+
+        //Test 14
+        //int _1Num = ReadInt64("Enter first six-digit number:", 6);
+        //int _2Num = ReadInt64("Enter second six-digit number:", 6);
+        //int _3Num = ReadInt64("Enter third six-digit number:", 6);
+        //int _4Num = ReadInt64("Enter fourth six-digit number:", 6);
+        //int _5Num = ReadInt64("Enter fifth six-digit number:", 6);
+        //int _6Num = ReadInt64("Enter sixth six-digit number:", 6);
+        //int sumNetice = _1Num + _2Num + _3Num + _4Num + _5Num + _6Num;
+        //int multNetice = _1Num * 1000000 + _3Num;
+        //int netice = (((sumNetice + multNetice) * 10 / 100) + _5Num + _6Num) * 11 / 100;
+        //Console.WriteLine(netice);
+        //Console.ReadKey();
+
+
+
+
+
+
+        //Test15
+        //int _1Num = ReadInt64("Enter first six-digit number:", 3);
+        //int _2Num = ReadInt64("Enter second six-digit number:", 3);
+        //int _3Num = ReadInt64("Enter third six-digit number:", 4);
+        //int _4Num = ReadInt64("Enter fourth six-digit number:", 4);
+        //int _5Num = ReadInt64("Enter fifth six-digit number:", 5);
+        //int _6Num = ReadInt64("Enter sixth six-digit number:", 5);
+        //int _7Num = ReadInt64("Enter seventh seven-digit number:", 6);
+        //int sum = ((((_1Num + _2Num + (_3Num * _4Num)) * 10 + 7) + _5Num + _6Num) - ((_1Num + _2Num) * 10 + 1)) + _7Num;
+        //int netice = ((((sum - (_1Num - _2Num) + (_3Num + _4Num)) * 18 / 100) * 3 / 100) * 1 / 100) + (_5Num + _6Num);
+        ////Console.WriteLine(netice);
+        ////Console.ReadKey();
+
+    }
+
+    static int ReadInt64(string caption, int digitLength)
+    {
+        Console.Write(caption);
+    label1:
+        string inputValue = Console.ReadLine();
+
+        int reqem;
+        if (int.TryParse(inputValue, out reqem) != true)
+        {
+            Console.WriteLine("reqem daxil etmediniz");
+            goto label1;
+        }
+
+        if (reqem >= Math.Pow(10, digitLength - 1) && reqem < Math.Pow(10, digitLength))
+        {
+            return reqem;
+        }
+        else
+        {
+            Console.WriteLine(digitLength + " reqemli daxil etmediniz");
+            goto label1;
+        }
+
+    }
+}
